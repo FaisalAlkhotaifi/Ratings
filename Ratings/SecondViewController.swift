e4ae9cd0d2d80e575dd62e5b60a7e5f083a3336e
Faisal Alkhotaifi <falkhotaifi@gmail.com>
2018-03-01 16:20:55 +0300

    Initial Commit

---
+++ Ratings/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Ratings
+//
+//  Created by Faisal Alkhotaifi on 3/1/18.
+//  Copyright © 2018 Faisal Alkhotaifi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
